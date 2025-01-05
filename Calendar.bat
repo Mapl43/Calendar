91ad06aa616ffd8d22cd9d8a313dfdf31f7df51e
AdoleHitlman <133385753+AdoleHitlman@users.noreply.github.com>
2025-01-05 23:16:09 +0300

    first commit

---
+++ Calendar.bat
@@ -0,0 +1,35 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFBZbWB2DAFujBb0d+uXL/eWNp04JR94ceYPe1LCPMtwa6UrqO58u2Ro=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSTk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJhSA==
+::ZQ05rAF9IBncCkqN+0xwdVsFLA==
+::ZQ05rAF9IAHYFVzEqQITKRdRQgCDLgs=
+::eg0/rx1wNQPfEVWB+kM9LVsJDCeDMG60ArQOiA==
+::fBEirQZwNQPfEVWB+kM9LVsJDCeDMG60ArQOiA==
+::cRolqwZ3JBvQF1fEqQITKRdRQgCDLgs=
+::dhA7uBVwLU+EWHqF+Ec+LBpGLA==
+::YQ03rBFzNR3SWATE10M8LRVQTRbi
+::dhAmsQZ3MwfNWATE10M8LRVQTRbi
+::ZQ0/vhVqMQ3MEVWAtB9wCxpYSQqGPXna
+::Zg8zqx1/OA3MEVWAtB9wCxpYSQqGPXna
+::dhA7pRFwIByZRRmn9U41Jh9VXmQ=
+::Zh4grVQjdCyDJGyX8VAjFBZbWB2DAFujBb0d+uXL/eWNp04JR94ceYPe1LCPMtww60nhe5IsxDRfgM5s
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+pip install -r requirements.txt
+python main.py
